f5010c072c90c0ed3d1cb62873851a71bceac01a
CristhiAn-555 <cristhianvalencia999@gmail.com>
2025-03-30 13:55:18 -0500

    subiendo la primera clase

---
+++ src/main/java/ec/edu/espoch/miprimerprogramaa/MiPrimerProgramaa.java
@@ -0,0 +1,98 @@
+
+package ec.edu.espoch.miprimerprogramaa;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class MiPrimerProgramaa {
+
+    public static void main(String[] args) {
+        System.out.println("Hola chicos PAO 2, Bienvenidos");
+
+        /* Manejo de variable*/
+        /*int  numUno = 5;
+        int numDos = 6;
+        int numTres= 3;
+
+        int resultado = numUno + numDos + numTres;
+        System.out.println("El rsultado:" + resultado);*/
+        
+       /* 
+        System.out.println("PARAMETROS DE VARIABLE BYTE");
+        System.out.println("Numero de bits:" + Byte.SIZE);
+        System.out.println("Numero de bytes:" + Byte.BYTES);
+        System.out.println("Valor maximo:" + Byte.MAX_VALUE);
+        System.out.println("Valor minimo:" + Byte.MIN_VALUE);
+        
+        byte x= 125;
+        
+        
+                
+        
+        System.out.println("PARAMETROS DE VARIABLE SHORT");
+        System.out.println("Numero de bits:" + Short.SIZE);
+        System.out.println("Numero de bytes:" + Short.BYTES);
+        System.out.println("Valor maximo:" + Short.MAX_VALUE);
+        System.out.println("Valor minimo:" + Short.MIN_VALUE);
+        
+        short y = 25454;
+        
+        System.out.println("PARAMETROS DE VARIABLE LONG");
+        System.out.println("Numero de bits:" + Long.SIZE);
+        System.out.println("Numero de bytes:" + Long.BYTES);
+        System.out.println("Valor maximo:" + Long.MAX_VALUE);
+        System.out.println("Valor minimo:" + Long.MIN_VALUE);
+        
+        long s = 45454545454L;
+        
+        System.out.println("PARAMETROS DE VARIABLE INT");
+        System.out.println("Numero de bits:" + Integer.SIZE);
+        System.out.println("Numero de bytes:" + Integer.BYTES);
+        System.out.println("Valor maximo:" + Integer.MAX_VALUE);
+        System.out.println("Valor minimo:" + Integer.MIN_VALUE);
+        int d= 4;
+        
+        
+        
+        System.out.println("PARAMETROS DE VARIABLE FLOAT");
+        System.out.println("Numero de bits:" + Float.SIZE);
+        System.out.println("Numero de bytes:" + Float.BYTES);
+        System.out.println("Valor maximo:" + Float.MAX_VALUE);
+        System.out.println("Valor minimo:" + Float.MIN_VALUE);
+        
+        float a= 3.2F;
+        
+        
+        System.out.println("PARAMETROS DE VARIABLE DOUBLE");
+        System.out.println("Numero de bits:" + Double.SIZE);
+        System.out.println("Numero de bytes:" + Double.BYTES);
+        System.out.println("Valor maximo:" + Double.MAX_VALUE);
+        System.out.println("Valor minimo:" + Double.MIN_VALUE);
+        
+        double c=3.6; 
+        */
+        
+        
+        /*String num= "5";
+        String num1= "3";
+        String num2= "5";
+        String num3= "9";
+        String num4= "1";
+        
+        int numA = 5;
+        int suma = Integer.parseInt(num)+Integer.parseInt(num1)+Integer.parseInt(num2)+Integer.parseInt(num3)+Integer.parseInt(num4);
+        
+        System.out.println("Resultado:" + suma);
+        */
+        
+       
+        
+        
+
+
+ 
+        
+
+    }
+}
